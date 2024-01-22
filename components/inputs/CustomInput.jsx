67216aca3df4e329a7c87b607417d90da5c5a045
Darius <dariuskumza@gmail.com>
2024-01-22 12:01:47 +0200

    Refactor CustomInput component styling

--- components/inputs/CustomInput.jsx
+++ components/inputs/CustomInput.jsx
@@ -1,19 +1,6 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-const shadow = {
-  borderWidth: 1,
-  borderRadius: 8,
-  borderColor: 'gray',
-  backgroundColor: '#fdf7ff',
-  // shadowColor: '#78747e',
-  // shadowOffset: {
-  //   width: 0,
-  //   height: 2,
-  // },
-  // shadowOpacity: 0.1,
-  // shadowRadius: 4,
-  // elevation: 3,
-};
+const shadow = {};
 
 export const CustomInput = ({ title, isHidden }) => {
   return (
@@ -38,7 +25,9 @@ const styles = StyleSheet.create({
     paddingVertical: 6,
     fontSize: 16,
     paddingHorizontal: 18,
-
-    ...shadow,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: 'gray',
+    backgroundColor: '#fdf7ff',
   },
 });
