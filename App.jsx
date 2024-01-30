8f0d2c20d529ed9ffa26a77384be6b3fb8f7c8e3
Darius <dariuskumza@gmail.com>
2024-01-30 20:42:24 +0200

    add stack navigation

--- App.jsx
+++ App.jsx
@@ -3,15 +3,23 @@ import { View, Text, SafeAreaView } from 'react-native';
 import { Layout } from './components/Layout';
 import { AuthCtxProvider } from './context/AuthContext';
 import { ProductContextProvider } from './context/ProductContext';
+import { NavigationContainer } from '@react-navigation/native';
+import SingleProductPage from './components/pages/products/SingleProductPage';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <AuthCtxProvider>
       <ProductContextProvider>
-        <View className="flex-1 bg-violet-50 w-full">
-          <Layout />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Layout" component={Layout} />
+            <Stack.Screen name="SingleProductPage" component={SingleProductPage} />
+          </Stack.Navigator>
           <StatusBar style="auto" />
-        </View>
+        </NavigationContainer>
       </ProductContextProvider>
     </AuthCtxProvider>
   );
--- components/pages/products/SingleProduct.jsx
+++ components/pages/products/SingleProduct.jsx
@@ -1,20 +1,36 @@
-import { View, Text, FlatList, Image, ScrollView, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
 
 const urlPrefix = 'http://10.0.2.2:3000';
 
 export default function SingleProduct({ prod }) {
+  const navigation = useNavigation();
+
   return (
-    <View className="border border-red-500 min-h-fit bg-gray-500">
-      <View className="">
-        <Image
-          className="h-[170px] w-[172px] rounded"
-          source={{ uri: `${urlPrefix}/${prod.img_url}` }}
-        ></Image>
+    <TouchableOpacity
+      onPress={() => navigation.navigate('SingleProductPage', { productId: prod.id })}
+    >
+      <View className="border border-gray-300 min-h-fit bg-violet-100">
+        <View className="">
+          <Image
+            className="h-[170px] w-[172px]"
+            source={{ uri: `${urlPrefix}/${prod.img_url}` }}
+          ></Image>
+        </View>
+        <View className="gap-1 px-4 py-2 pb-2 h-18">
+          <Text className="font-semibold text-xl ">{prod.price} €</Text>
+          <Text className="">{prod.title}</Text>
+        </View>
       </View>
-      <View className="gap-1 px-4 py-2 pb-2 h-18">
-        <Text className="font-semibold text-xl text-white">{prod.price} €</Text>
-        <Text className=" text-lg text-white">{prod.title}</Text>
-      </View>
-    </View>
+    </TouchableOpacity>
   );
 }
