ac87abce80228205d1f2368675a5c30e6c47441b
Darius <dariuskumza@gmail.com>
2024-02-06 18:04:59 +0200

    shows when updated

--- components/pages/products/SingleProduct.jsx
+++ components/pages/products/SingleProduct.jsx
@@ -6,7 +6,17 @@ const urlPrefix = 'http://10.0.2.2:3000';
 export default function SingleProduct({ prod }) {
   // console.log(prod.id);
   const navigation = useNavigation();
-  console.log(prod.img_url);
+  // console.log(prod.img_url);
+
+  const differenceInMilliseconds = new Date() - new Date(prod.updated);
+  const differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
+
+  let updatedTime;
+  if (differenceInDays < 1) {
+    updatedTime = `1 hours ago`;
+  } else {
+    updatedTime = `${differenceInDays.toFixed(2)} days ago`;
+  }
 
   return (
     <TouchableOpacity onPress={() => navigation.navigate('SingleProductPage', { product: prod })}>
@@ -18,12 +28,15 @@ export default function SingleProduct({ prod }) {
               source={{ uri: `${urlPrefix}/${prod.img_url}` }}
             />
           ) : (
-            <Text className="h-[170px] w-[172px] text-center pt-4 bg-white text-gray-400">
+            <Text className="h-[170px] w-[172px] text-center pt-8 bg-white text-gray-400 text-xl">
               No Image
             </Text>
           )}
+          <View className=" bg-violet-400 w-fit absolute px-2 right-0">
+            <Text className="text-center py-1 text-xs ">{updatedTime}</Text>
+          </View>
         </View>
-        <View className="gap-1 px-4 py-2 pb-2 h-18">
+        <View className="gap-1 px-4 py-2 pb-2 h-18 ">
           <Text className="font-semibold text-xl ">{prod.price} €</Text>
           <Text className="">{prod.title}</Text>
         </View>
